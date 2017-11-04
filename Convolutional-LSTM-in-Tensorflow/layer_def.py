3cfa7f6987565878bab0aeef8b444e49e0368603
Ki Hyun Won <kithwon@naver.com>
2017-11-03 18:03:19 -0700

    composite model

--- Convolutional-LSTM-in-Tensorflow/layer_def.py
+++ Convolutional-LSTM-in-Tensorflow/layer_def.py
@@ -1,4 +1,5 @@
 
+
 """functions used to construct different architectures  
 """
 
@@ -12,10 +13,8 @@ tf.app.flags.DEFINE_float('weight_decay', 0.0005,
 xinitializer = tf.contrib.layers.xavier_initializer(uniform=True)
 def _activation_summary(x):
   """Helper to create summaries for activations.
-
   Creates a summary that provides a histogram of activations.
   Creates a summary that measure the sparsity of activations.
-
   Args:
     x: Tensor
   Returns:
@@ -27,12 +26,10 @@ def _activation_summary(x):
 
 def _variable_on_cpu(name, shape, initializer):
   """Helper to create a Variable stored on CPU memory.
-
   Args:
     name: name of the variable
     shape: list of ints
     initializer: initializer for Variable
-
   Returns:
     Variable Tensor
   """
@@ -43,17 +40,14 @@ def _variable_on_cpu(name, shape, initializer):
 
 def _variable_with_weight_decay(name, shape, stddev, wd):
   """Helper to create an initialized Variable with weight decay.
-
   Note that the Variable is initialized with a truncated normal distribution.
   A weight decay is added only if one is specified.
-
   Args:
     name: name of the variable
     shape: list of ints
     stddev: standard deviation of a truncated Gaussian
     wd: add L2Loss weight decay multiplied by this float. If None, weight
         decay is not added for this Variable.
-
   Returns:
     Variable Tensor
   """
@@ -67,8 +61,11 @@ def _variable_with_weight_decay(name, shape, stddev, wd):
     tf.add_to_collection('losses', weight_decay)
   return var
 
-def conv_layer(inputs, kernel_size, stride, num_features, idx, linear = False):
-  with tf.variable_scope('{0}_conv'.format(idx)) as scope:
+def lrelu(x, leak=0.2, name="lrelu"):
+  return tf.maximum(x, leak*x)
+
+def conv2d(inputs, kernel_size, stride, num_features, name, linear=False):
+  with tf.variable_scope(name) as scope:
     input_channels = inputs.get_shape()[3]
 
     weights = _variable_with_weight_decay('weights', shape=[kernel_size,kernel_size,input_channels,num_features],stddev=0.01, wd=FLAGS.weight_decay)
@@ -78,7 +75,7 @@ def conv_layer(inputs, kernel_size, stride, num_features, idx, linear = False):
     conv_biased = tf.nn.bias_add(conv, biases)
     if linear:
       return conv_biased
-    conv_rect = tf.nn.elu(conv_biased,name='{0}_conv'.format(idx))
+    conv_rect = tf.nn.elu(conv_biased, name=name)
     return conv_rect
 
 def transpose_conv_layer(inputs, kernel_size, stride, num_features, idx, linear = False):
@@ -128,8 +125,8 @@ def transpose_conv_layer_1D(inputs, kernel_size, stride, num_features, idx, line
     conv_rect = tf.nn.elu(conv_biased,name='{0}_transpose_conv'.format(idx))
     return conv_rect
 
-def fc_layer(inputs, hiddens, idx, flat = False, linear = False):
-  with tf.variable_scope('{0}_fc'.format(idx)) as scope:
+def fc_layer(inputs, hiddens, name, flat=False, linear=False):
+  with tf.variable_scope(name) as scope:
     input_shape = inputs.get_shape().as_list()
     if flat:
       dim = input_shape[1]*input_shape[2]*input_shape[3]
@@ -138,11 +135,11 @@ def fc_layer(inputs, hiddens, idx, flat = False, linear = False):
       dim = input_shape[1]
       inputs_processed = inputs
     
-    weights = _variable_with_weight_decay('weights', shape=[dim,hiddens],stddev=FLAGS.weight_init, wd=FLAGS.weight_decay)
-    biases = tf.get_variable('biases', [hiddens], initializer=tf.constant_initializer(FLAGS.weight_init))
+    weights = _variable_with_weight_decay('weights', shape=[dim,hiddens],stddev=0.01, wd=FLAGS.weight_decay)
+    biases = tf.get_variable('biases', [hiddens], initializer=tf.constant_initializer(0.01))
     if linear:
-      return tf.add(tf.matmul(inputs_processed,weights),biases,name=str(idx)+'_fc')
+      return tf.add(tf.matmul(inputs_processed,weights),biases,name=name)
   
     ip = tf.add(tf.matmul(inputs_processed,weights),biases)
-    return tf.nn.elu(ip,name=str(idx)+'_fc')
-
+    return tf.nn.elu(ip,name=name)
+    
--- Convolutional-LSTM-in-Tensorflow/main_1D.py
+++ Convolutional-LSTM-in-Tensorflow/main_1D.py
@@ -1,3 +1,4 @@
+
 from __future__ import print_function
 import os.path
 import time
@@ -9,6 +10,7 @@ from fits_reader import *
 import bouncing_balls as b
 import layer_def as ld
 from ConvLSTM1D import BasicConvLSTMCell
+from BasicConvLSTMCell2d import BasicConvLSTMCell2d
 import matplotlib.pyplot as plt
 FLAGS = tf.app.flags.FLAGS
 
@@ -18,7 +20,7 @@ tf.app.flags.DEFINE_string('data_dir', './Data',
                             """dir to load data""")
 tf.app.flags.DEFINE_integer('seq_length', 16,
                             """size of hidden layer""")
-tf.app.flags.DEFINE_integer('seq_start', 5,
+tf.app.flags.DEFINE_integer('seq_start', 8,
                             """ start of seq generation""")
 tf.app.flags.DEFINE_integer('max_step', 200000,
                             """max num of steps""")
@@ -29,7 +31,7 @@ tf.app.flags.DEFINE_float('lr', .001,
 tf.app.flags.DEFINE_integer('batch_size', 32,
                             """batch size for training""")
 tf.app.flags.DEFINE_boolean('resume', False,
-                            """whether to load saved weights""")
+                            """whether to load saved wieghts""")
 #fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v') 
 
 def generate_bouncing_ball_sample(batch_size, seq_length, shape, num_balls):
@@ -38,8 +40,18 @@ def generate_bouncing_ball_sample(batch_size, seq_length, shape, num_balls):
     dat[i, :, :, :, :] = b.bounce_vec(32, num_balls, seq_length)
   return dat 
 
-def lrelu(x, leak=0.2, name="lrelu"):
-  return tf.maximum(x, leak*x)
+def generate_x_batch(batch_size, t, f, n_sig=2, ret=False, noise=True):
+    f_0 = tf.random_uniform(shape=[batch_size, n_sig,1,1],minval=0., maxval=512.) #in ms 
+    amp = tf.random_uniform(shape=[batch_size,n_sig,1,1],minval=0.25,maxval= 2.5)
+    width = tf.random_uniform(shape=[batch_size,n_sig,1,1],minval=1., maxval=20.)
+    slope = tf.random_uniform(shape=[batch_size,n_sig,1,1],minval=-15, maxval=15)
+    f0_all = f_0 + slope * t
+    pulse = tf.reduce_sum(amp*tf.exp(-0.5 * (f - f0_all) ** 2 / width ** 2.), axis=1)
+    if noise:
+        noise_level = 0.2
+        pulse += noise_level *tf.random_uniform(pulse.get_shape(), minval=0, maxval=1)
+    return pulse[..., tf.newaxis]
+
 def encode_stack(inputs, i):
   conv1 = ld.conv_layer_1D(inputs, 3, 2, 8, "encode_{}".format(i+1))
   # conv2
@@ -52,7 +64,7 @@ def decode_stack(inputs, i):
   conv2 = ld.transpose_conv_layer_1D(conv1, 3, 1, 8, "decode_{}".format(i+2))
   return conv2
 
-def network(inputs, hidden, lstm=True):
+def network(inputs, hidden, lstm_depth=4):
   #inputs is 3D tensor (batch, )
   conv1 = ld.conv_layer_1D(inputs, 5, 2, 8, "encode_1")
   # conv2
@@ -63,16 +75,23 @@ def network(inputs, hidden, lstm=True):
   conv = ld.conv_layer_1D(conv, 1, 1, 4, "encode_{}".format(i+3))
   y_0 = conv
   
-  if lstm:
-    # conv lstm cell 
-    with tf.variable_scope('conv_lstm', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
+
+  with tf.variable_scope('conv_lstm_0', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
     #with tf.variable_scope('conv_lstm', initializer = tf.random_uniform_initializer(-.01, 0.1)):
       cell = BasicConvLSTMCell([16,], [5,], 4)
       if hidden is None:
         hidden = cell.zero_state(FLAGS.batch_size, tf.float32) 
-      y_1, hidden = cell(y_0, hidden)
-  else:
-    y_1 = ld.conv_layer_1D(y_0, 3, 1, 8, "encode_{}".format(i+4))
+      y_0, _ = cell(y_0, hidden)
+
+
+  for l in range(1, lstm_depth):
+    # conv lstm cell 
+    with tf.variable_scope('conv_lstm_{}'.format(l), initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
+    #with tf.variable_scope('conv_lstm', initializer = tf.random_uniform_initializer(-.01, 0.1)):
+      cell = BasicConvLSTMCell([16,], [5,], 4)
+      y_0, _ = cell(y_0, hidden)
+
+  y_1 = y_0
   #import IPython; IPython.embed()
   dconv = ld.transpose_conv_layer_1D(y_1, 1, 1, 8, "decode_1")
   #import IPython; IPython.embed()
@@ -85,46 +104,58 @@ def network(inputs, hidden, lstm=True):
 
   return x_1, hidden
 
-def anomaly_network(inputs, encoder_state_1, encoder_state_2, encoder_state_3):
+def network_2d(inputs, encoder_state, past_state, future_state):
   #inputs is 3D tensor (batch, )
-  conv = ld.conv_layer_1D(inputs, 5, 2, 8, "encode")
+  conv = ld.conv2d(inputs, 8, 2, 8, "encode")
   
   # encoder convlstm 
   with tf.variable_scope('conv_lstm_encoder_1', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
-    cell1 = BasicConvLSTMCell.BasicConvLSTMCell([256,], [5,], 3)
-    if encoder_state_1 is None:
-      encoder_state_1 = cell1.zero_state(FLAGS.batch_size, tf.float32) 
-    conv, encoder_state_1 = cell1(conv, encoder_state_1)
+    cell1 = BasicConvLSTMCell2d([4, 256], [8, 8], 4)
+    if encoder_state is None:
+      encoder_state = cell1.zero_state(FLAGS.batch_size, tf.float32) 
+    conv1, encoder_state = cell1(conv, encoder_state)
   with tf.variable_scope('conv_lstm_encoder_2', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
-    cell2 = BasicConvLSTMCell.BasicConvLSTMCell([256,], [5,], 3)
-    if encoder_state_2 is None:
-      encoder_state_2 = cell2.zero_state(FLAGS.batch_size, tf.float32) 
-    conv, encoder_state_2 = cell2(conv, encoder_state_2)
+    cell2 = BasicConvLSTMCell2d([4, 256], [8, 8], 4)
+    conv2, encoder_state = cell2(conv1, encoder_state)
   with tf.variable_scope('conv_lstm_encoder_3', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
-    cell3 = BasicConvLSTMCell.BasicConvLSTMCell([256,], [5,], 3)
-    if encoder_state_3 is None:
-      encoder_state_3 = cell3.zero_state(FLAGS.batch_size, tf.float32) 
-    conv, encoder_state_3 = cell3(conv, encoder_state_3)
+    cell3 = BasicConvLSTMCell2d([4, 256], [8, 8], 4)
+    conv3, encoder_state = cell3(conv2, encoder_state)
   
-  # decoder convlstm 
-  with tf.variable_scope('conv_lstm_decoder_1', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
-    dcell1 = BasicConvLSTMCell.BasicConvLSTMCell([256,], [5,], 3)
-    dconv1, _ = dcell1(conv, encoder_state_1)
-  with tf.variable_scope('conv_lstm_decoder_2', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
-    dcell2 = BasicConvLSTMCell.BasicConvLSTMCell([256,], [5,], 3)
-    dconv2, _ = dcell2(dconv1, encoder_state_2)
-  with tf.variable_scope('conv_lstm_decoder_3', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
-    dcell3 = BasicConvLSTMCell.BasicConvLSTMCell([256,], [5,], 3)
-    dconv3 = tf.add(dconv1, dconv2)
-    dconv, _ = dcell3(dconv3, encoder_state_3)
+  # past decoder convlstm 
+  with tf.variable_scope('past_decoder_1', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
+    pcell1 = BasicConvLSTMCell2d([4, 256], [8, 8], 4)
+    if past_state is None:
+      past_state = pcell1.zero_state(FLAGS.batch_size, tf.float32) 
+    pconv1, past_state = pcell1(conv1, past_state)
+  with tf.variable_scope('past_decoder_2', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
+    pcell2 = BasicConvLSTMCell2d([4, 256], [8, 8], 4)
+    pconv2, past_state = pcell2(conv2, past_state)
+  with tf.variable_scope('past_decoder_3', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
+    pcell3 = BasicConvLSTMCell2d([4, 256], [8, 8], 4)
+    pconv3, past_state = pcell3(conv3, past_state)
 
-  # output
-  x_1 = ld.transpose_conv_layer_1D(dconv, 5, 2, 1, "output", True)
-  
-  return x_1, encoder_state_1, encoder_state_2, encoder_state_3
+  # future decoder convlstm 
+  with tf.variable_scope('future_decoder_1', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
+    fcell1 = BasicConvLSTMCell2d([4, 256], [8, 8], 4)
+    if future_state is None:
+      future_state = fcell1.zero_state(FLAGS.batch_size, tf.float32) 
+    fconv1, future_state = fcell1(conv1, future_state)
+  with tf.variable_scope('future_decoder_2', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
+    fcell2 = BasicConvLSTMCell2d([4, 256], [8, 8], 4)
+    fconv2, future_state = fcell2(conv2, future_state)
+  with tf.variable_scope('future_decoder_3', initializer=tf.contrib.layers.xavier_initializer(uniform=True)):
+    fcell3 = BasicConvLSTMCell2d([4, 256], [8, 8], 4)
+    fconv3, future_state = fcell3(conv3, future_state)
+
+  # present output
+  x_1 = ld.transpose_conv_layer(pconv3, 8, 2, 1, "present_output", True)
+  # future output
+  y_1 = ld.transpose_conv_layer(fconv3, 8, 2, 1, "future_output", True)
+
+  return x_1, y_1, encoder_state, past_state, future_state
 
 # make a template for reuse
-network_template = tf.make_template('stacked_network', anomaly_network)
+network_template = tf.make_template('network', network_2d)
 
 def _plot_samples(samples, fname):
     batch_size = samples.shape[0]
@@ -137,58 +168,35 @@ def _plot_samples(samples, fname):
     print('saving', fname)
     plt.savefig(fname)
 
-def train():
+def train(with_gan=True, load_x=True):
   """Train ring_net for a number of steps."""
   with tf.Graph().as_default():
-    # make inputs
     x = tf.placeholder(tf.float32, [None, FLAGS.seq_length, 512, 1])
+
 
     # possible dropout inside
     keep_prob = tf.placeholder("float")
     #x_dropout = tf.nn.dropout(x, keep_prob)
     x_dropout = x
 
-    # create network
-    x_unwrap = []
-
     # conv network
-    encoder_state_1 = None
-    encoder_state_2 = None
-    encoder_state_3 = None
-    for i in range(FLAGS.seq_length-1):
-      if i < FLAGS.seq_start:
-        x_1, encoder_state_1, encoder_state_2, encoder_state_3 = network_template(x_dropout[:,i,:,:], encoder_state_1, encoder_state_2, encoder_state_3)
-      else:
-        x_1, encoder_state_1, encoder_state_2, encoder_state_3 = network_template(x_1, encoder_state_1, encoder_state_2, encoder_state_3)
-      x_unwrap.append(x_1)
+    encoder_state = None
+    past_state = None
+    future_state = None
+    x_1, y_1, encoder_state, past_state, future_state = network_template(x_dropout[:,:FLAGS.seq_start,:,:], encoder_state, past_state, future_state)
 
-    # pack them all together 
-    x_unwrap = tf.stack(x_unwrap)
-    x_unwrap = tf.transpose(x_unwrap, [1,0,2,3])
-
-    # this part will be used for generating images
-    x_unwrap_g = []
-    encoder_state_1g = None
-    encoder_state_2g = None
-    encoder_state_3g = None
-    for i in range(32):
-      if i < FLAGS.seq_start:
-        x_1_g, encoder_state_1g, encoder_state_2g, encoder_state_3g = network_template(x_dropout[:,i,:,:], encoder_state_1g, encoder_state_2g, encoder_state_3g)
-      else:
-        x_1_g, encoder_state_1g, encoder_state_2g, encoder_state_3g = network_template(x_1_g, encoder_state_1g, encoder_state_2g, encoder_state_3g)
-      x_unwrap_g.append(x_1_g)
-
-    # pack them generated ones
-    x_unwrap_g = tf.stack(x_unwrap_g)
-    x_unwrap_g = tf.transpose(x_unwrap_g, [1,0,2,3])
-
-    # calc total loss (compare x_t to x_t+1)
-    loss = tf.nn.l2_loss(x[:,FLAGS.seq_start+1:,:,:] - x_unwrap[:,FLAGS.seq_start:,:,:])
+    past_loss = tf.nn.l2_loss(x[:,:FLAGS.seq_start,:,:] - x_1)
+    future_loss = tf.nn.l2_loss(x[:,FLAGS.seq_start:,:,:] - y_1)
+    loss = past_loss + future_loss
     tf.summary.scalar('loss', loss)
 
     # training
-    train_op = tf.train.AdamOptimizer(FLAGS.lr).minimize(loss)
-    
+    optimizer = tf.train.AdamOptimizer(FLAGS.lr)
+    gvs = optimizer.compute_gradients(loss)
+    # gradient clipping
+    capped_gvs = [(tf.clip_by_value(grad, -3., 3.), var) for grad, var in gvs]
+    train_op = optimizer.apply_gradients(capped_gvs)
+
     # List of all Variables
     variables = tf.global_variables()
 
@@ -216,21 +224,21 @@ def train():
       saver.restore(sess, latest)
     else:
       print("init network from scratch")
+    
     # Summary op
     graph_def = sess.graph.as_graph_def(add_shapes=True)
     summary_writer = tf.summary.FileWriter(FLAGS.train_dir, graph_def=graph_def)
     files = find_files(FLAGS.data_dir)
     sample_dir = FLAGS.train_dir + '/samples/'
+    
     if not os.path.exists(sample_dir):
       os.makedirs(sample_dir)
-    for step in xrange(FLAGS.max_step):
-      #dat = generate_bouncing_ball_sample(FLAGS.batch_size, FLAGS.seq_length, 32, FLAGS.num_balls)
+    for step in range(FLAGS.max_step):
       dat = load_batch(FLAGS.batch_size, files, step)
-      #import IPython; IPython.embed()
       t = time.time()
       _, loss_r = sess.run([train_op, loss],feed_dict={x:dat, keep_prob:FLAGS.keep_prob})
       elapsed = time.time() - t
-      #print(step)
+      
       if step%100 == 0 and step != 0:
         summary_str = sess.run(summary_op, feed_dict={x:dat, keep_prob:FLAGS.keep_prob})
         summary_writer.add_summary(summary_str, step) 
@@ -245,20 +253,19 @@ def train():
         saver.save(sess, checkpoint_path, global_step=step)  
         print("saved to " + FLAGS.train_dir)
 
-        # make video
         print("now saving sample!")
-        dat_gif = dat
-        ims = sess.run(x_unwrap_g,feed_dict={x:dat_gif, keep_prob:FLAGS.keep_prob})
-        _plot_samples(ims.squeeze(), sample_dir+'step_{}.png'.format(step))
-
+        im_x, im_y = sess.run([x_1, y_1],feed_dict={x:dat, keep_prob:FLAGS.keep_prob})
+        _plot_samples(dat[:,:FLAGS.seq_start,:,:].squeeze(), sample_dir+'step_{}_past_t.png'.format(step))
+        _plot_samples(im_x.squeeze(), sample_dir+'step_{}_past.png'.format(step))
+        _plot_samples(dat[:,FLAGS.seq_start:,:,:].squeeze(), sample_dir+'step_{}_future_t.png'.format(step))
+        _plot_samples(im_y.squeeze(), sample_dir+'step_{}_future.png'.format(step))
 
 def main(argv=None):  # pylint: disable=unused-argument
-  if tf.gfile.Exists(FLAGS.train_dir):
-    tf.gfile.DeleteRecursively(FLAGS.train_dir)
-  tf.gfile.MakeDirs(FLAGS.train_dir)
+  if not FLAGS.resume:
+    if tf.gfile.Exists(FLAGS.train_dir):
+      tf.gfile.DeleteRecursively(FLAGS.train_dir)
+    tf.gfile.MakeDirs(FLAGS.train_dir)
   train()
 
 if __name__ == '__main__':
   tf.app.run()
-
-
